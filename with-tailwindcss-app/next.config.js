4bea9876982bcbb7ed7f961acc154063e8c57ed8
JoseSolorio <lodisolito@gmail.com>
2022-11-16 21:55:02 -0800

    updated logo and color

--- with-tailwindcss-app/next.config.js
+++ with-tailwindcss-app/next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["links.papareact.com",],
+    domains: ["links.papareact.com","www.hourwork.com"],
   },
   env: {
     mapbox_key: "pk.eyJ1Ijoic29saXQwMTIzIiwiYSI6ImNsYWs1bmpnejBkZngzb281NHk3ZzFvdGIifQ.P8BNh-mSHbvvr_-O8SYIVg"
