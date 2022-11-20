902fc19e0814f45999acf9d817365b04635359b8
JoseSolorio <lodisolito@gmail.com>
2022-11-20 03:05:45 -0800

    removed clickable card on search page
    so that the heart is clickable.

--- with-tailwindcss-app/pages/search.js
+++ with-tailwindcss-app/pages/search.js
@@ -22,6 +22,7 @@ const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
 const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
 const range = `${formattedStartDate} - ${formattedEndDate}`;
 
+
   return (
     <div className="">
         <Header placeholder={`${location} | ${range } | ${noOfGuests} guests`}/>
@@ -29,8 +30,7 @@ const range = `${formattedStartDate} - ${formattedEndDate}`;
         <main className="flex ">
             <section className="flex-grow pt-14 px-6 ">
                 <p className="text-xs">300+ stays for <b >{range}</b> for <b>{noOfGuests}</b> guests.</p>
-                <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
-
+                <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>  
                 <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                     <p className="button">Cancellation</p>
                     <p className="button">Type of place</p>
@@ -39,7 +39,8 @@ const range = `${formattedStartDate} - ${formattedEndDate}`;
                     
                     <div className="">
                         {searchResults.map(({img, location, title, description, star, price, total}) => (
-                            <Link href={"/post/" + title}>   <InfoCard key={img} img={img} location={location} title={title} description={description} star={star} price={price} total={total}/>  </Link> 
+                            // <Link href={"/post/" + title}>   </Link>
+                            <InfoCard key={img} img={img} location={location} title={title} description={description} star={star} price={price} total={total} />  
                         ))}
                     </div>
 
